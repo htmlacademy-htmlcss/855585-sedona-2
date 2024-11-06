cc69b36f8340b10a9e8e6b858138413e4629a8db
lenapokrovskaya <64478411+lenapokrovskaya@users.noreply.github.com>
2024-11-06 15:33:54 +0300

    Дополняет проверку по критериям

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
 const overlay = document.querySelector('.overlay')
-const popup = document.querySelector('.modal-container');
-const openPopupButton = document.querySelector('.button-open');
-const closePopupButton = document.querySelector('.modal-close-button');
+const popup = document.querySelector('.modal__container');
+const openPopupButton = document.querySelector('.button--open');
+const closePopupButton = document.querySelector('.modal__close-button');
 
 //  Modal-window
 
 openPopupButton.addEventListener('click', function (evt) {
   evt.preventDefault();
   overlay.style.display = "block";
-  popup.classList.add('modal-container-open');
+  popup.classList.add('modal__container--open');
 });
 
 closePopupButton.addEventListener('click', function () {
   overlay.style.display = "none";
-  popup.classList.remove('modal-container-open');
+  popup.classList.remove('modal__container--open');
 });
 
 document.addEventListener('keydown', function (event) {
   if (event.key === 'Escape') {
     overlay.style.display = "none";
-    popup.classList.remove('modal-container-open');
+    popup.classList.remove('modal__container--open');
   }
 });
