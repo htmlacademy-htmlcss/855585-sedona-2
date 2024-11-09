5b1ac168e441ace44e1cc0b70f3379ecc381666a
lenapokrovskaya <64478411+lenapokrovskaya@users.noreply.github.com>
2024-11-09 17:10:22 +0300

    Дорабатывает проект для допуска

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const overlay = document.querySelector('.overlay')
 const popup = document.querySelector('.modal__container');
-const openPopupButton = document.querySelector('.button--open');
+const openPopupButton = document.querySelector('.js-button-open');
 const closePopupButton = document.querySelector('.modal__close-button');
 
 //  Modal-window
@@ -8,17 +8,17 @@ const closePopupButton = document.querySelector('.modal__close-button');
 openPopupButton.addEventListener('click', function (evt) {
   evt.preventDefault();
   overlay.style.display = "block";
-  popup.classList.add('modal__container--open');
+  popup.classList.add('js-container-open');
 });
 
 closePopupButton.addEventListener('click', function () {
   overlay.style.display = "none";
-  popup.classList.remove('modal__container--open');
+  popup.classList.remove('js-container-open');
 });
 
 document.addEventListener('keydown', function (event) {
   if (event.key === 'Escape') {
     overlay.style.display = "none";
-    popup.classList.remove('modal__container--open');
+    popup.classList.remove('js-container-open');
   }
 });
